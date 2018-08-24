4e0f6c85885f45e2103de542410cb0129511fe15
moto <mnxamoto@mail.ru>
2018-08-24 18:43:10 +0300

    Проект загружен

---
+++ 2048/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace _2048
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(403, 403);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 5;
+            // 
+            // Column3
+            // 
+            this.Column3.Frozen = true;
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 200;
+            // 
+            // Column4
+            // 
+            this.Column4.Frozen = true;
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 403);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximumSize = new System.Drawing.Size(419, 441);
+            this.MinimumSize = new System.Drawing.Size(419, 441);
+            this.Name = "Form1";
+            this.Text = "2048";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ 2048/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Zaderjka = 500, Q = 1;
+        Random rnd = new Random();
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(4);
+
+            for (int i = 0; i < 4; i++)
+            {
+                dataGridView1.Columns[i].Width = 100;
+                dataGridView1.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                dataGridView1.Rows[i].Height = 100;
+            }
+
+            dataGridView1.ClearSelection();
+            this.Focus();
+
+            New4islo();
+        }
+
+        public void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.Enabled = false;
+
+            if (e.KeyCode == Keys.Up)
+            {
+                SdvigPoVertikali(0);
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                SdvigPoVertikali(-3);
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                SdvigPoGorizontali(0);
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                SdvigPoGorizontali(-3);
+            }
+
+            if (e.KeyCode == Keys.F5)
+            {
+                SaveRecord();
+            }
+
+            if (e.KeyCode == Keys.F6)
+            {
+                if (Q == 1)
+                {
+                    Zaderjka = 0;
+                    this.Text = "2048 (Zaderjka = 0)";
+                    Q = 0;
+                }
+                else
+                {
+                    Zaderjka = 500;
+                    this.Text = "2048 (Zaderjka = 500)";
+
+                    Q = 1;
+                }
+            }
+
+            this.Enabled = true;
+        }
+
+        private void SaveRecord()
+        {
+            int Summa = 0, Naib = 2;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Summa += Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+
+                    if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) > Naib)
+                    {
+                        Naib = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                    }
+                }
+            }
+
+            System.IO.File.AppendAllText(System.IO.Path.GetDirectoryName(Application.ExecutablePath.ToString()) + "\\Таблица рекордов.txt", DateTime.Now.ToString() + " " + Convert.ToString(Naib) + " " + Convert.ToString(Summa) + "\r\n");
+            System.Diagnostics.Process.Start(System.IO.Path.GetDirectoryName(Application.ExecutablePath.ToString()) + "\\Таблица рекордов.txt");
+        }
+
+        private void New4islo()
+        {
+            int x = rnd.Next(4), y = rnd.Next(4), Pystoe = 0;
+            if (dataGridView1.Rows[x].Cells[y].Value == null)
+            {
+                dataGridView1.Rows[x].Cells[y].Value = Math.Pow(2, rnd.Next(2) + 1);
+            }
+            else
+            {
+                Pystoe = 0;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (dataGridView1.Rows[i].Cells[j].Value == null)
+                        {
+                            Pystoe++;
+                        }
+                    }
+                }
+
+                if (Pystoe == 0)
+                {
+                    SaveRecord();
+                    this.Close();
+                }
+
+                New4islo();
+            }
+        }
+
+        async void SdvigPoVertikali(int I)
+        {
+            for (int ii = 0; ii < 4; ii++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (dataGridView1.Rows[Math.Abs(I + i)].Cells[j].Value == null)
+                        {
+                            dataGridView1.Rows[Math.Abs(I + i)].Cells[j].Value = dataGridView1.Rows[Math.Abs(I + i + 1)].Cells[j].Value;
+                            dataGridView1.Rows[Math.Abs(I + i + 1)].Cells[j].Value = null;
+                            
+                        }
+                        else
+                        {
+                            if (Convert.ToString(dataGridView1.Rows[Math.Abs(I + i)].Cells[j].Value) == Convert.ToString(dataGridView1.Rows[Math.Abs(I + i + 1)].Cells[j].Value))
+                            {
+                                dataGridView1.Rows[Math.Abs(I + i)].Cells[j].Style.BackColor = Color.Lime;
+                                dataGridView1.Rows[Math.Abs(I + i + 1)].Cells[j].Style.BackColor = Color.Lime;
+                                await Task.Delay(Zaderjka);
+                                dataGridView1.Rows[Math.Abs(I + i)].Cells[j].Value = Convert.ToInt32(dataGridView1.Rows[Math.Abs(I + i)].Cells[j].Value) * 2;
+                                dataGridView1.Rows[Math.Abs(I + i + 1)].Cells[j].Value = null;
+                                dataGridView1.Rows[Math.Abs(I + i + 1)].Cells[j].Style.BackColor = Color.White;
+                                await Task.Delay(Zaderjka);
+                                dataGridView1.Rows[Math.Abs(I + i)].Cells[j].Style.BackColor = Color.White;
+                            }
+                        }
+                    }
+                }
+            }
+            New4islo();
+        }
+
+        async void SdvigPoGorizontali(int I)
+        {
+            for (int ii = 0; ii < 4; ii++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (dataGridView1.Rows[j].Cells[Math.Abs(I + i)].Value == null)
+                        {
+                            dataGridView1.Rows[j].Cells[Math.Abs(I + i)].Value = dataGridView1.Rows[j].Cells[Math.Abs(I + i + 1)].Value;
+                            dataGridView1.Rows[j].Cells[Math.Abs(I + i + 1)].Value = null;
+                        }
+                        else
+                        {
+                            if (Convert.ToString(dataGridView1.Rows[j].Cells[Math.Abs(I + i)].Value) == Convert.ToString(dataGridView1.Rows[j].Cells[Math.Abs(I + i + 1)].Value))
+                            {
+                                dataGridView1.Rows[j].Cells[Math.Abs(I + i)].Style.BackColor = Color.Lime;
+                                dataGridView1.Rows[j].Cells[Math.Abs(I + i + 1)].Style.BackColor = Color.Lime;
+                                await Task.Delay(Zaderjka);
+                                dataGridView1.Rows[j].Cells[Math.Abs(I + i)].Value = Convert.ToInt32(dataGridView1.Rows[j].Cells[Math.Abs(I + i)].Value) * 2;
+                                dataGridView1.Rows[j].Cells[Math.Abs(I + i + 1)].Value = null;
+                                dataGridView1.Rows[j].Cells[Math.Abs(I + i + 1)].Style.BackColor = Color.White;
+                                await Task.Delay(Zaderjka);
+                                dataGridView1.Rows[j].Cells[Math.Abs(I + i)].Style.BackColor = Color.White;
+                            }
+                        }
+                    }
+                }
+            }
+            New4islo();
+        }
+    }
+}
